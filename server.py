186b6e93c51a34ca8ab1631abcd828ee87e272c1
Kenneth Reitz <me@kennethreitz.org>
2017-06-04 12:49:56 -0400

    server supports graphene endpoint

--- server.py
+++ server.py
@@ -1,16 +1,27 @@
-from flask import Flask
+import graphene
+from flask import Flask, jsonify
 from flask_common import Common
+
+
+
+class Query(graphene.ObjectType):
+    hello = graphene.String(name=graphene.Argument(graphene.String, default_value="stranger"))
+
+    def resolve_hello(self, args, context, info):
+        return 'Hello ' + args['name']
+
+schema = graphene.Schema(query=Query)
 
 app = Flask(__name__)
 app.debug = True
 
 common = Common(app)
 
-@app.route('/')
+@app.route('/<hello>')
 @common.cache.cached(timeout=50)
-def hello():
-    return 'Hello World!'
-
+def hello(hello='world'):
+    result = schema.execute('{ hello }')
+    return jsonify(result.data)
 
 if __name__ == '__main__':
     common.serve()
